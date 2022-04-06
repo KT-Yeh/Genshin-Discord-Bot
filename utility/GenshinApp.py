af0e736870fc609050a7d0a2c19cec818f2101d4
KT <xns77477@gmail.com>
2022-04-06 10:37:21 +0800

    修改error handle邏輯

--- utility/GenshinApp.py
+++ utility/GenshinApp.py
@@ -15,16 +15,15 @@ class GenshinApp:
             with open('data/user_data.json', 'r', encoding="utf-8") as f:
                 self.__user_data = json.loads(f.read())
         except:
-            self.__user_data = { } 
+            self.__user_data = { }
 
     async def setCookie(self, user_id: str, cookie: str) -> str:
         """設定使用者Cookie
         :param user_id: 使用者Discord ID
         :cookie: Hoyolab cookie
         """
+        log.info(f'setCookie(user_id={user_id}, cookie={cookie}') 
         user_id = str(user_id)
-        log.info(f'{user_id} Cookie設置:{cookie}')
-        
         # 從Cookie確認是否有ltuid, ltoken, cookie_token, account_id
         if any(key not in cookie for key in ('cookie_token', 'ltuid', 'ltoken', 'account_id')):
             return '無效的Cookie，請重新輸入正確的Cookie'
@@ -33,31 +32,30 @@ class GenshinApp:
         client.set_cookies(cookie)
         try:
             accounts = await client.genshin_accounts()
-            await client.close()
         except genshin.errors.GenshinException as e:
             log.error(e.msg)
-            return '無效的Cookie，請重新輸入正確的Cookie'
+            result = '無效的Cookie，請重新輸入正確的Cookie'
         else:
-            if  len(accounts) == 0:
+            if len(accounts) == 0:
                 log.info('帳號內沒有任何角色')
-                return '錯誤，該帳號內沒有任何角色'
-
-            self.__user_data[user_id] = {}
-            self.__user_data[user_id]['cookie'] = cookie
-            log.info(f'{user_id}的Cookie設置成功:{cookie}')
-            log.info(f'{user_id}共有{len(accounts)}個角色')
-            
-            if len(accounts) == 1:
-                self.setUID(user_id, accounts[0].uid)
-                return 'Cookie設定完成！'
+                result = '發生錯誤，該帳號內沒有任何角色'
             else:
-                message = f'```帳號內共有{len(accounts)}個角色\n'
-                for account in accounts:
-                    log.info(f'UID:{account.uid} 等級:{account.level} 角色名字:{account.nickname}')
-                    message += f'UID:{account.uid} 等級:{account.level} 角色名字:{account.nickname}\n'
-                message += f'```\n請用`{config.bot_prefix}uid`指定要保存的角色(例: `{config.bot_prefix}uid 812345678`)'
-                self.__saveUserData()
-                return message
+                self.__user_data[user_id] = {}
+                self.__user_data[user_id]['cookie'] = cookie
+                log.info(f'{user_id}的Cookie設置成功')
+                
+                if len(accounts) == 1:
+                    self.setUID(user_id, accounts[0].uid)
+                    result = 'Cookie設定完成！'
+                else:
+                    result = f'```帳號內共有{len(accounts)}個角色\n'
+                    for account in accounts:
+                        result += f'UID:{account.uid} 等級:{account.level} 角色名字:{account.nickname}\n'
+                    result += f'```\n請用`{config.bot_prefix}uid`指定要保存的角色(例: `{config.bot_prefix}uid 812345678`)'
+                    self.__saveUserData()
+        finally:
+            await client.close()
+            return result
     
     def setUID(self, user_id: str, uid: str) -> bool:
         """設定原神UID，當帳號內有多名角色時，保存指定的UID
@@ -92,18 +90,19 @@ class GenshinApp:
         try:
             account = await task1
             notes = await task2
-            await client.close()
         except genshin.errors.DataNotPublic as e:
             log.error(e.msg)
-            return '即時便箋未開啟\n請從HOYOLAB網頁或App開啟即時便箋功能'
+            result = '即時便箋功能未開啟\n請從HOYOLAB網頁或App開啟即時便箋功能'
         except genshin.errors.GenshinException as e:
             log.error(e.msg)
-            return e.msg
-
-        result = f'{account.nickname} {self.__server_dict[account.region]} {uid.replace(uid[3:-3], "***", 1)}\n'
-        result += f'--------------------\n'
-        result += self.__parseNotes(notes)
-        return result
+            result = e.msg
+        else:
+            result = f'{account.nickname} {self.__server_dict[account.region]} {uid.replace(uid[3:-3], "***", 1)}\n'
+            result += f'--------------------\n'
+            result += self.__parseNotes(notes)
+        finally:
+            await client.close()
+            return result
     
     async def redeemCode(self, user_id: str, code: str) -> str:
         """為使用者使用指定的兌換碼
@@ -118,12 +117,14 @@ class GenshinApp:
         client = self.__getGenshinClient(user_id)
         try:
             await client.redeem_code(code, self.__user_data[user_id]['uid'])
-            await client.close()
         except genshin.errors.GenshinException as e:
             log.error(f'{e.msg}')
-            return e.msg
+            result = e.msg
         else:
-            return '兌換碼使用成功！'
+            result = '兌換碼使用成功！'
+        finally:
+            await client.close()
+            return result
     
     async def claimDailyReward(self, user_id: str) -> str:
         """為使用者在Hoyolab簽到
@@ -137,11 +138,13 @@ class GenshinApp:
         client = self.__getGenshinClient(user_id)
         try:
             reward = await client.claim_daily_reward()
-            await client.close()
         except genshin.AlreadyClaimed:
-            return '今日獎勵已經領過了！'
+            result = '今日獎勵已經領過了！'
         else:
-            return f'Hoyolab今日簽到成功！獲得 {reward.amount}x {reward.name}'
+            result = f'Hoyolab今日簽到成功！獲得 {reward.amount}x {reward.name}'
+        finally:
+            await client.close()
+            return result
 
     async def getSpiralAbyss(self, user_id: str, uid: str = None, previous: bool = False, full_data: bool = False) -> Union[str, discord.Embed]:
         """取得深境螺旋資訊
@@ -160,30 +163,32 @@ class GenshinApp:
         client = self.__getGenshinClient(user_id)
         try:
             abyss = await client.get_spiral_abyss(uid, previous=previous)
-            await client.close()
         except genshin.errors.GenshinException as e:
             log.error(e.msg)
-            return e.msg
-        
-        embed = discord.Embed(title=f'深境螺旋第 {abyss.season} 期戰績', color=0x7fbcf5)
-        embed.add_field(
-            name=f'最深抵達：{abyss.max_floor}　戰鬥次數：{abyss.total_battles}　★：{abyss.total_stars}', 
-            value=f'統計週期：{abyss.start_time.strftime("%Y.%m.%d")} ~ {abyss.end_time.strftime("%Y.%m.%d")}', 
-            inline=False
-        )
-        for floor in abyss.floors:
-            if full_data == False and floor is not abyss.floors[-1]:
-                continue
-            for chamber in floor.chambers:
-                name = f'{floor.floor}-{chamber.chamber}　★{chamber.stars}'
-                # 取得深淵上下半層角色名字
-                chara_list = [[], []]
-                for i, battle in enumerate(chamber.battles):
-                    for chara in battle.characters:
-                        chara_list[i].append(getCharacterName(chara))
-                value = f'[{".".join(chara_list[0])}]／[{".".join(chara_list[1])}]'
-                embed.add_field(name=name, value=value)
-        return embed
+            result = e.msg
+        else:
+            result = discord.Embed(title=f'深境螺旋第 {abyss.season} 期戰績', color=0x7fbcf5)
+            result.add_field(
+                name=f'最深抵達：{abyss.max_floor}　戰鬥次數：{abyss.total_battles}　★：{abyss.total_stars}', 
+                value=f'統計週期：{abyss.start_time.strftime("%Y.%m.%d")} ~ {abyss.end_time.strftime("%Y.%m.%d")}', 
+                inline=False
+            )
+            # 取得深淵每一層資料
+            for floor in abyss.floors:
+                if full_data == False and floor is not abyss.floors[-1]:
+                    continue
+                for chamber in floor.chambers:
+                    name = f'{floor.floor}-{chamber.chamber}　★{chamber.stars}'
+                    # 取得深淵上下半層角色名字
+                    chara_list = [[], []]
+                    for i, battle in enumerate(chamber.battles):
+                        for chara in battle.characters:
+                            chara_list[i].append(getCharacterName(chara))
+                    value = f'[{".".join(chara_list[0])}]／[{".".join(chara_list[1])}]'
+                    result.add_field(name=name, value=value)
+        finally:
+            await client.close()
+            return result
     
     async def getTravelerDiary(self, user_id: str, month: str) -> Union[str, discord.Embed]:
         """取得使用者旅行者札記
@@ -198,31 +203,32 @@ class GenshinApp:
         client = self.__getGenshinClient(user_id)
         try:
             diary = await client.get_diary(self.__user_data[user_id]['uid'], month=month)
-            await client.close()
         except genshin.errors.GenshinException as e:
             log.error(e.msg)
-            return e.msg
-        
-        d = diary.data
-        embed = discord.Embed(
-            title=f'{diary.nickname}的旅行者札記：{month}月',
-            description=f'原石收入比上個月{"增加" if d.primogems_rate > 0 else "減少"}了{abs(d.primogems_rate)}%，摩拉收入比上個月{"增加" if d.mora_rate > 0 else "減少"}了{abs(d.mora_rate)}%',
-            color=0xfd96f4
-        )
-        embed.add_field(
-            name='當月共獲得', 
-            value=f'原石：{d.current_primogems}　上個月：{d.last_primogems}\n'
-                  f'摩拉：{d.current_mora}　上個月：{d.last_mora}',
-            inline=False
-        )
-        # 將札記原石組成平分成兩個field
-        for i in range(0, 2):
-            msg = ''
-            length = len(d.categories)
-            for j in range(round(length/2*i), round(length/2*(i+1))):
-                msg += f'{d.categories[j].name[0:2]}：{d.categories[j].percentage}%\n'
-            embed.add_field(name=f'原石收入組成 {i+1}', value=msg, inline=True)
-        return embed
+            result = e.msg
+        else:    
+            d = diary.data
+            result = discord.Embed(
+                title=f'{diary.nickname}的旅行者札記：{month}月',
+                description=f'原石收入比上個月{"增加" if d.primogems_rate > 0 else "減少"}了{abs(d.primogems_rate)}%，摩拉收入比上個月{"增加" if d.mora_rate > 0 else "減少"}了{abs(d.mora_rate)}%',
+                color=0xfd96f4
+            )
+            result.add_field(
+                name='當月共獲得', 
+                value=f'原石：{d.current_primogems}　上個月：{d.last_primogems}\n'
+                    f'摩拉：{d.current_mora}　上個月：{d.last_mora}',
+                inline=False
+            )
+            # 將札記原石組成平分成兩個field
+            for i in range(0, 2):
+                msg = ''
+                length = len(d.categories)
+                for j in range(round(length/2*i), round(length/2*(i+1))):
+                    msg += f'{d.categories[j].name[0:2]}：{d.categories[j].percentage}%\n'
+                result.add_field(name=f'原石收入組成 {i+1}', value=msg, inline=True)
+        finally:
+            await client.close()
+            return result
     
     def __checkUserData(self, user_id: str, *,checkUserID = True, checkCookie = True, checkUID = True) -> Tuple[bool, str]:
         if checkUserID and user_id not in self.__user_data.keys():
