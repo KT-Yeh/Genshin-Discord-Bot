87b15fa9667348fa146781de7871d84e39ddbb04
KT <xns77477@gmail.com>
2022-04-07 13:02:04 +0800

    log將warning以上的紀錄額外輸出到data/error.log檔案

--- utility/utils.py
+++ utility/utils.py
@@ -3,11 +3,16 @@ import genshin
 import re
 from data.character_names import character_names
 
-logging.basicConfig(
-    level=logging.INFO,
-    format='%(asctime)s %(levelname)s %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+__file_handler = logging.FileHandler('data/error.log', encoding='utf-8')
+__file_handler.setLevel(logging.WARNING)
+__console_handler = logging.StreamHandler()
+__console_handler.setLevel(logging.INFO)
+__formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s', '%Y-%m-%d %H:%M:%S')
+__file_handler.setFormatter(__formatter)
+__console_handler.setFormatter(__formatter)
+logging.getLogger().setLevel(logging.INFO)
+logging.getLogger().addHandler(__file_handler)
+logging.getLogger().addHandler(__console_handler)
 log = logging
 
 def getCharacterName(character: genshin.models.BaseCharacter) -> str:
