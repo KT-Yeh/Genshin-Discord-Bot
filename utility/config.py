5a07bf1c5f16914998da2b46798724baf77136de
KT <xns77477@gmail.com>
2022-04-11 20:11:38 +0800

    dict改為get取值

--- utility/config.py
+++ utility/config.py
@@ -6,14 +6,14 @@ class Config:
         project_path = Path(__file__).parents[1]
         config_path = project_path.joinpath('config.json') 
         with open(config_path, encoding='utf-8') as file:
-            data = json.loads(file.read())
+            data: dict = json.loads(file.read())
         
-        self.application_id = data['application_id']
-        self.test_server_id = data['test_server_id']
-        self.bot_token = data['bot_token']
-        self.bot_prefix = data['bot_prefix']
-        self.bot_cooldown = data['bot_cooldown']
-        self.auto_daily_reward_time = data['auto_daily_reward_time']
-        self.auto_check_resin_threshold = data['auto_check_resin_threshold']
+        self.application_id = data.get('application_id')
+        self.test_server_id = data.get('test_server_id')
+        self.bot_token = data.get('bot_token')
+        self.bot_prefix = data.get('bot_prefix')
+        self.bot_cooldown = data.get('bot_cooldown')
+        self.auto_daily_reward_time = data.get('auto_daily_reward_time')
+        self.auto_check_resin_threshold = data.get('auto_check_resin_threshold')
 
 config = Config()
