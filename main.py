413f50a4638e135ca4b42a76c1074d230f7df606
KT <xns77477@gmail.com>
2022-04-15 20:01:20 +0800

    移除help、移除舊指令冷卻時間的設定

--- main.py
+++ main.py
@@ -1,23 +1,16 @@
 import discord
 from discord.ext import commands
 from pathlib import Path
-from utility.CustomHelp import custom_help
 from utility.config import config
 from utility.utils import log
 
-# 設定使用者呼叫指定的冷卻時間(秒數)
-default_cooldown = commands.Cooldown(1, config.bot_cooldown)
 intents = discord.Intents.default()
 class GenshinDiscordBot(commands.Bot):
     def __init__(self):
         super().__init__(
             command_prefix=config.bot_prefix,
             intents=intents,
-            help_command=custom_help,
-            application_id=config.application_id,
-            description=f'Hello，原神小幫手的指令前綴為"{config.bot_prefix}"\n'
-                        f'第一次使用原神指令前必需先設置Cookie(請參閱 {config.bot_prefix}help cookie)\n'
-                        f'小幫手全部指令如下：'
+            application_id=config.application_id
         )
 
     async def setup_hook(self) -> None:
@@ -34,13 +27,9 @@ class GenshinDiscordBot(commands.Bot):
     async def on_ready(self):
         log.info(f'You have logged in as {client}')
         log.info(f'Total {len(self.guilds)} servers connected')
-        for command in self.commands:
-            command._buckets._cooldown = default_cooldown
         await self.change_presence(activity=discord.Game(name='Genshin Impact'))
 
     async def on_command_error(self, ctx, error):
-        if isinstance(error, commands.CommandNotFound):
-            return
         if isinstance(error, commands.MissingRequiredArgument):
             await ctx.send(f'指令缺少必要參數，請使用 `{config.bot_prefix}help {ctx.command}` 查看使用方式')
 
