06d8c9194e1030f37f459387161aa6b3c9a7b6b6
KT <xns77477@gmail.com>
2022-06-11 20:14:15 +0800

    getUID會更新最後使用時間

--- utility/GenshinApp.py
+++ utility/GenshinApp.py
@@ -101,6 +101,7 @@ class GenshinApp:
     
     def getUID(self, user_id: str) -> Union[int, None]:
         if user_id in self.__user_data.keys():
+            user_last_use_time.update(user_id)
             return int(self.__user_data[user_id].get('uid'))
         return None
 
