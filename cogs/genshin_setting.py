d3acc9e02629545403130e5a97ce2f47f5fd5dd4
KT <xns77477@gmail.com>
2022-07-06 23:00:09 +0800

    UID設定改為下拉選單、調整部分文本

--- cogs/genshin_setting.py
+++ cogs/genshin_setting.py
@@ -1,11 +1,14 @@
 import discord
+import genshin
 import asyncio
 import sentry_sdk
+import typing
 from discord import app_commands
 from discord.app_commands import Choice
 from discord.ext import commands
 from utility.GenshinApp import genshin_app
 from utility.config import config
+from utility.utils import getServerName
 
 class Setting(commands.Cog, name='設定'):
     def __init__(self, bot):
@@ -19,7 +22,7 @@ class Setting(commands.Cog, name='設定'):
             style=discord.TextStyle.long,
             required=True,
             min_length=100,
-            max_length=1500
+            max_length=2000
         )
         async def on_submit(self, interaction: discord.Interaction):
             try:
@@ -43,39 +46,46 @@ class Setting(commands.Cog, name='設定'):
         Choice(name='③ 顯示小幫手Cookie使用與保存告知', value=2)])
     async def slash_cookie(self, interaction: discord.Interaction, option: int):
         if option == 0:
-            help_msg = (
-            "1.先複製本文最底下整段程式碼\n"
-            "2.PC或手機使用Chrome開啟Hoyolab登入帳號 <https://www.hoyolab.com>\n"
-            "3.在網址列先輸入 `java`，然後貼上程式碼，確保網址開頭變成 `javascript:`\n"
-            "4.按Enter，網頁會變成顯示你的Cookie，全選然後複製\n"
-            "5.在這裡提交結果，使用：`/cookie設定 提交已取得的Cookie`\n"
-            "· 遇到問題嗎？點連結看其他方法：<https://bit.ly/3LgQkg0>\n"
-            "https://i.imgur.com/OQ8arx0.gif")
+            embed = discord.Embed(color=0x7289da, description=
+                "1.先複製本文最底下整段程式碼\n"
+                "2.PC或手機使用Chrome開啟 [Hoyolab網頁](https://www.hoyolab.com) 並登入帳號\n"
+                "3.在網址列先輸入 `java`，然後貼上程式碼，確保網址開頭變成 `javascript:`\n"
+                "4.按Enter，網頁會變成顯示你的Cookie，全選然後複製\n"
+                "5.在這裡使用：`/cookie設定 提交已取得的Cookie`\n"
+                "· 遇到問題嗎？點 [教學連結](https://bit.ly/3LgQkg0) 看其他方法\n"
+            )
+            embed.set_image(url="https://i.imgur.com/OQ8arx0.gif")
             code_msg = "```script:d=document.cookie; c=d.includes('account_id') || alert('過期或無效的Cookie,請先登出帳號再重新登入!'); c && document.write(d)```"
-            await interaction.response.send_message(content=help_msg)
+            await interaction.response.send_message(embed=embed)
             await interaction.followup.send(content=code_msg)
         elif option == 1:
             await interaction.response.send_modal(self.CookieModal())
         elif option == 2:
             msg = ('· Cookie的內容包含你個人的識別代碼，不包含帳號與密碼\n'
                 '· 因此無法用來登入遊戲，也無法更改帳密，Cookie內容大概長這樣：'
-                '`ltoken=xxxx ltuid=1234 cookie_token=yyyy account_id=1122`\n'
+                '`ltoken=xxxx ltuid=1234 cookie_token=yyyy account_id=1234`\n'
                 '· 小幫手保存並使用Cookie是為了在Hoyolab網站上取得你的原神資料並提供服務\n'
                 '· 小幫手將資料保存於雲端主機獨立環境，只與Discord、Hoyolab伺服器連線\n'
-                '· 更詳細說明可以點擊小幫手個人檔案到巴哈說明文查看，若仍有疑慮請不要使用小幫手\n'
+                '· 更詳細說明可以到 [巴哈說明文](https://forum.gamer.com.tw/Co.php?bsn=36730&sn=162433) 查看，若仍有疑慮請不要使用小幫手\n'
                 '· 當提交Cookie給小幫手時，表示你已同意小幫手保存並使用你的資料\n'
                 '· 你可以隨時刪除保存在小幫手的資料，請使用 `/清除資料` 指令\n')
-            embed = discord.Embed(title='小幫手Cookie使用與保存告知', description=msg)
-            await interaction.response.send_message(embed=embed)
-
-    # 選擇欲保存的UID按鈕
-    class UidButton(discord.ui.Button):
-        def __init__(self, uid: int):
-            super().__init__(style=discord.ButtonStyle.primary, label=str(uid))
-            self.uid = str(uid)
+            embed = discord.Embed(title='小幫手Cookie使用與保存告知', color=0x7289da, description=msg)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
+    
+    # 選擇欲保存UID的下拉選單
+    class UidDropdown(discord.ui.Select):
+        def __init__(self, accounts: typing.Sequence[genshin.models.GenshinAccount]):
+            options = [discord.SelectOption(
+                    label=f"[{getServerName(str(account.uid)[0])}] {account.uid}",
+                    description=f"Lv.{account.level} {account.nickname}",
+                    value=str(i)
+                ) for i, account in enumerate(accounts)
+            ]
+            super().__init__(placeholder='請選擇要保存的UID：', options=options)
+            self.accounts = accounts
         
-        async def callback(self, interaction: discord.Interaction) -> None:
-            msg = genshin_app.setUID(str(interaction.user.id), self.uid)
+        async def callback(self, interaction: discord.Interaction):
+            msg = genshin_app.setUID(str(interaction.user.id), str(self.accounts[int(self.values[0])].uid))
             await interaction.response.edit_message(content=msg, view=None)
 
     # 設定原神UID，當帳號內有多名角色時，保存指定的UID
@@ -90,14 +100,8 @@ class Setting(commands.Cog, name='設定'):
             await interaction.edit_original_message(content=str(e))
         else:
             view = discord.ui.View(timeout=config.discord_view_short_timeout)
-            msg = '```'
-            for account in accounts:
-                view.add_item(self.UidButton(account.uid))
-                msg += f'UID:{account.uid} 等級:{account.level} 角色名字:{account.nickname}\n'
-            msg += '```\n請選擇要保存的UID：'
-            await interaction.edit_original_message(content=msg, view=view)
-            await view.wait()
-            await interaction.edit_original_message(view=None)
+            view.add_item(self.UidDropdown(accounts))
+            await interaction.edit_original_message(view=view)
 
     # 清除資料確認按紐
     class ConfirmButton(discord.ui.View):
