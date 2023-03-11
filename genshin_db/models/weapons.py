153e9a3d7190238dc5f7e5123d7594daf94ae253
KT <xns77477@gmail.com>
2023-03-11 10:26:43 +0800

    Fix: 修正某些武器精煉不為 5 導致出錯的問題
    問題：
    - 59 行：list index out of range，因為 r1 與其他 list 長度不一樣
    
    修正：
    - 存取前檢查每個 list 的長度

--- genshin_db/models/weapons.py
+++ genshin_db/models/weapons.py
@@ -56,14 +56,18 @@ class Weapon(GenshinDbBase):
     @property
     def effect_desciption(self) -> str:
         """取得包含精煉數值的武器特效敘述"""
+        refines = [self.r1, self.r2, self.r3, self.r4, self.r5]
         nums = len(self.r1)  # 要填的數值數量
         refine_values: List[str] = []
+        # 將 r1~r5 數值合併成一個 str，然後放入 refine_values
         for i in range(nums):
-            refine_values.append(
-                "/".join([self.r1[i], self.r2[i], self.r3[i], self.r4[i], self.r5[i]])
-            )
-        result = self.effect
+            refine_values.append(str(refines[0][i]))
+            for j in range(1, 5):
+                # 有些武器的最高精煉等級不同，所以要檢查
+                if i < len(refines[j]):
+                    refine_values[i] += f"/{refines[j][i]}"
 
+        result = self.effect
         for i in range(nums):
             # 將 {0}, {1}... 替換成對應數值
             result = result.replace(f"{{{i}}}", refine_values[i])
