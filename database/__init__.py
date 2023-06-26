9155595a69f72f3cf0a564b4b0874654dd0ad35b
KT <xns77477@gmail.com>
2023-06-26 19:35:26 +0800

    fix: 修正 database import 不相關 class

--- database/__init__.py
+++ database/__init__.py
@@ -1,4 +1,14 @@
-from .app import *
+from .app import Database
+from .dataclass import *
 from .migration import migrate
-from .models import *
+from .models import (
+    Base,
+    GenshinScheduleNotes,
+    GenshinShowcase,
+    GenshinSpiralAbyss,
+    ScheduleDailyCheckin,
+    StarrailScheduleNotes,
+    StarrailShowcase,
+    User,
+)
 from .tools import Tool
